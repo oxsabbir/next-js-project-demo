dff515e731c368bee0b9c565c854a7930d3ec6bb
Sabbir Hossain <sabbir2always@gmail.com>
2024-01-24 15:41:56 +0600

    Dashboard is created and active link is working now setting up the posgresql database for the next operation

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Customer (){
+    return <div className="bg-gray-400 text-white p-8">
+        <h2 className=" text-center uppercase text-xl">Invoice Page</h2>
+    </div>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Invocie (){
+    return <div className="bg-green-400 text-white p-8">
+        <h2>Invoice Page</h2>
+    </div>
+}
